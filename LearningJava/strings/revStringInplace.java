18ddbfdd566d45af7219beca7a3b371b2759e480
anshshah23 <ansh.shah082@svkmmumbai.onmicrosoft.com>
2025-06-05 23:48:03 +0530

    reverse string in place

---
+++ LearningJava/strings/revStringInplace.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class revStringInplace {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+        while(start<end){
+            char temp = s[end];
+            s[end--] = s[start];
+            s[start++] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        revStringInplace solution = new revStringInplace();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(s);
+        System.out.println(s); // Output: ['o', 'l', 'l', 'e', 'h']
+    }
+}
