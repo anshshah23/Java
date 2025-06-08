00bf6a47b08d468e2caea368f3c47f7adcb9f6fb
anshshah23 <ansh.shah082@svkmmumbai.onmicrosoft.com>
2025-06-08 11:41:59 +0530

    Implement recursive stack sorting algorithm

---
+++ LearningJava/stack/sortStackRecursive.java
@@ -0,0 +1,41 @@
+/*Complete the function below*/
+class sortStackRecursive {
+    public Stack<Integer> sort(Stack<Integer> s) {
+        // add code here.
+        Stack<Integer> ans = new Stack<>();
+        while(!s.isEmpty()) {
+            int top = s.pop();
+            if(ans.isEmpty() || ans.peek() > top){
+                insertAtBottom(ans, top);
+            }
+            else{
+                ans.push(top);
+            }
+        }
+        return ans;
+    }
+    private static void insertAtBottom(Stack<Integer> ans, int ele){
+        if(ans.isEmpty() || ele > ans.peek()) {
+            ans.push(ele);
+        } else {
+            int top = ans.pop();
+            insertAtBottom(ans, ele);
+            ans.push(top);
+        }
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(3);
+        stack.push(31);
+        stack.push(98);
+        stack.push(92);
+        
+        sortStackRecursive sorter = new sortStackRecursive();
+        Stack<Integer> sortedStack = sorter.sort(stack);
+        
+        while (!sortedStack.isEmpty()) {
+            System.out.println(sortedStack.pop());
+        }
+    }
+}
