b55503e598e7caf6c2f598071e2bf65c6cb0f2e7
anshshah23 <ansh.shah082@svkmmumbai.onmicrosoft.com>
2025-06-22 23:56:02 +0530

    Add validParenthesisStack class with isValid method implementation

---
+++ LearningJava/stack/validParenthesisStack.java
@@ -0,0 +1,32 @@
+package stack;
+class validParenthesisStack {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            } else {
+                if(st.isEmpty()) return false;
+                char top = st.peek();
+                if (st.peek() == '(' && a == ')') {
+                    st.pop();
+                } else if (st.peek() == '{' && a == '}') {
+                    st.pop();
+                } else if (st.peek() == '[' && a == ']') {
+                    st.pop();
+                }
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+    public static void main(String[] args) {
+        validParenthesisStack vps = new validParenthesisStack();
+        System.out.println(vps.isValid("()")); // true
+        System.out.println(vps.isValid("()[]{}")); // true
+        System.out.println(vps.isValid("(]")); // false
+        System.out.println(vps.isValid("([)]")); // false
+        System.out.println(vps.isValid("{[]}")); // true
+    }
+}
